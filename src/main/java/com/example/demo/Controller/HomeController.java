b7892c926d6b683ffb9f26732617ba1345ea58a5
Anzhelika Lebedeva <fikfa_@mail.ru>
2019-07-13 22:45:55 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello World");
+    }
+
+    @GetMapping("/hello1")
+    public String getHelloMessage(){
+        return "Hello Spring !";
+    }
+
+}
